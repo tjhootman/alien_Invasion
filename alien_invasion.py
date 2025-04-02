d39f3d9579bed08b759dccbc96d69b1b8d738cdc
tjhootman <tjhootman@gmail.com>
2025-04-01 20:33:07 -0400

    Create a ship class and set up to draw to screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 
@@ -21,6 +22,8 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
+        self.ship = Ship(self)
+
     def run_game(self):
         # game loop
         while self.running:
@@ -31,6 +34,7 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.blit(self.bg, (0,0))
+            self.ship.draw()
             pygame.display.flip()
             self.clock.tick(self.settings.FPS)
 
